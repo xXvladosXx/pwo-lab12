b4bdcb86cdce29860abc206c7e08ff4d717af151
Vladyslav <vladyslav.hutsenko@pollub.edu.pl>
2022-10-15 18:49:05 +0200

    Wykonanie 3 zadań z przykładowymi wynikami

---
+++ src/main/java/pwo/utils/SequenceGenerator.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package pwo.utils;
+
+/**
+ * @author pidoras
+ */
+import java.math.BigDecimal;
+
+public interface SequenceGenerator {
+
+    void reset();
+
+    BigDecimal nextTerm();
+
+    BigDecimal getTerm(int i);
+}
