eb85cf9d71ad17d9fd75e9c6304fbc38d3906f35
Vladyslav <vladyslav.hutsenko@pollub.edu.pl>
2022-10-15 19:20:37 +0200

    Dystrybucja 1

---
+++ src/main/java/pwo/app/SeqToOutApp.java
@@ -0,0 +1,40 @@
+package pwo.app;
+import pwo.utils.SequenceTools;
+
+public class SeqToOutApp extends SeqToFileApp {
+
+    @Override
+    protected boolean getArgs(String[] args) {
+
+        if (super.getArgs(args)) return true;
+
+        return seqType != null && from >= 0 && to >= 0;
+    }
+
+
+    @Override
+    protected boolean wirteSeq() {
+
+        System.out.println(SequenceTools.getTermsAsColumn(
+                seqType.getGenerator(), from, to));
+
+        return true;
+    }
+
+
+    @Override
+    public void run(String[] args) {
+
+        System.out.println("Sequence to terminal CLI app");
+
+        if (!getArgs(args)) {
+            System.out.println("!Illegal arguments\n"
+                    + "Legal usage: seqName from to");
+            return;
+        }
+
+        wirteSeq();
+
+    }
+
+}
